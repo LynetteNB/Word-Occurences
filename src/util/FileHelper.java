5e0e91e2403c0f75cb68e6f8f56cefb54e1f0caa
Lynette <lyna8907@gmail.com>
2018-06-05 14:29:32 -0500

    created FileHelper class to read and write files

---
+++ src/util/FileHelper.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHelper {
+    /**
+     * Takes in a String with the filepath of the file to be read and returns a list with each line of the file as an element of the list.
+     * @param filePath The filepath to the file to be read.
+     * @return List of strings with each line read from the file as an element of the list.
+     */
+    public static List<String> readFile(String filePath) {
+        List<String> fileLines = new ArrayList<>();
+        StringBuilder paragraph = new StringBuilder("");
+        try {
+            fileLines = Files.readAllLines(Paths.get(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileLines;
+    }
+
+    /**
+     * Creates a new file with the given contents to the path given.
+     * @param filePath The name of the file to be created including file path.
+     * @param contents A list of the contents to be written in the new file.
+     */
+    public static void outputFile(String filePath, List<String> contents){
+        try {
+            if(!Files.exists(Paths.get(filePath).getParent())){
+                Files.createDirectory(Paths.get(filePath).getParent());
+            }
+            Files.createFile(Paths.get(filePath));
+            Files.write(Paths.get(filePath), contents);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        outputFile("new/output.txt", readFile("input.txt"));
+    }
+}
