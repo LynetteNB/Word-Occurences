058cb1cce56ef1de054af590a9af2bca7a3291d7
Lynette <lyna8907@gmail.com>
2018-06-05 22:16:52 -0500

    added comment to runner

--- src/HistogramRunner.java
+++ src/HistogramRunner.java
@@ -1,5 +1,8 @@
 import models.ParagraphHistogram;
 
+/**
+ * Creates new Paragraph Histogram object.
+ */
 public class HistogramRunner {
     public static void main(String[] args) {
         ParagraphHistogram ph = new ParagraphHistogram("input.txt", "output.txt");
